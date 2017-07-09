90e027b582dfffa511e4eccd02da348aae666fec
Grace Feng <g.feng@sap.com>
2017-07-09 13:58:06 +0800

    put the class Hero below the import statement, before @Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 
-@Component({
-	selector: 'app-root',
-	// templateUrl: './app.component.html',
-	template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
-	styleUrls: ['./app.component.css']
-})
 export class Hero {
 	id: number;
 	name: string;
 }
 
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+
 export class AppComponent {
 	title = 'Tour of Heroes';
-	hero = 'windstorm';
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
 }
