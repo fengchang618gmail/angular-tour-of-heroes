801bb2ba66bc1ae9f137c1b38b4e907b14b27939
Grace Feng <g.feng@sap.com>
2017-07-09 13:35:06 +0800

    Create a Hero class with id and name properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	// templateUrl: './app.component.html',
+	template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
+	styleUrls: ['./app.component.css']
 })
+export class Hero {
+	id: number;
+	name: string;
+}
+
 export class AppComponent {
-  title = 'Tour of Heroes';
-  hero = 'windstorm';
+	title = 'Tour of Heroes';
+	hero = 'windstorm';
 }
