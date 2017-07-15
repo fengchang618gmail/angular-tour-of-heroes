0e98c063aad30e1faf8ea4a54bd1cc4dcb252260
Grace Feng <g.feng@sap.com>
2017-07-15 20:13:10 +0800

    http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -21,6 +26,8 @@ import { AppRoutingModule } from './app-routing.module';
 	imports: [
 		BrowserModule,
 		FormsModule, // <-- import the FromsModule before binding with [(ngModel)]
+		HttpModule,
+		InMemoryWebApiModule.forRoot(InMemoryDataService),
 		AppRoutingModule
 	],
 	providers: [
