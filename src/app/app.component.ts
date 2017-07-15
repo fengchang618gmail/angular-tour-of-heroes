22fb7cc544fad0888fe72f62f7e19959ebfa11d6
Grace Feng <g.feng@sap.com>
2017-07-15 13:07:27 +0800

    inject the HeroService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
-
+import { HeroService } from './hero.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	styleUrls: ['./app.component.css'],
+	providers: [HeroService]
 })
 
 export class AppComponent {
 	title = 'Tour of Heroes';
 	heroes: Hero[];
 	selectedHero: Hero;
+
+	constructor (private heroService: HeroService) { }
 
 	onSelected(hero: Hero): void {
 		this.selectedHero = hero;
